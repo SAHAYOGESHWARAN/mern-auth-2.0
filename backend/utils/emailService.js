6da12e859745d05bab4df08fa8e1cc71dc5c0654
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-09-14 08:39:15 +0530

    ma

---
+++ backend/utils/emailService.js
@@ -0,0 +1,22 @@
+const nodemailer = require('nodemailer');
+
+const sendEmail = async (options) => {
+  const transporter = nodemailer.createTransport({
+    service: 'Gmail',
+    auth: {
+      user: process.env.EMAIL,
+      pass: process.env.EMAIL_PASSWORD,
+    },
+  });
+
+  const mailOptions = {
+    from: 'no-reply@yourapp.com',
+    to: options.email,
+    subject: options.subject,
+    html: options.message,
+  };
+
+  await transporter.sendMail(mailOptions);
+};
+
+module.exports = sendEmail;
