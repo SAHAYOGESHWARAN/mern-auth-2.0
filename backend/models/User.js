9e29c9ca0142deb7edc9d977f81f884eab75591c
SAHAYOGESHWARAN <sahayogeshwaran1@gmail.com>
2024-09-14 20:40:12 +0530

    ma

--- backend/models/User.js
+++ backend/models/User.js
@@ -2,14 +2,27 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  isVerified: { type: Boolean, default: false },
-  resetPasswordToken: String,
-  resetPasswordExpires: Date,
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  otp: {
+    type: String,
+    required: false,
+  },
+  verified: {
+    type: Boolean,
+    default: false,
+  }
 });
 
-userSchema.pre('save', async function(next) {
+// Hash password before saving the user
+userSchema.pre('save', async function (next) {
   if (!this.isModified('password')) return next();
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
